9662feca5bd97cd9213f079fed474613e61e262f
taaem <hipsteeeer@web.de>
2015-04-01 14:39:12 +0200

    Initial Commit

---
+++ node-install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Node Linux Installer by www.github.com/taaem"
+echo "Need Root for installing NodeJS"
+sudo sh -c 'echo "Got Root!"' 
+
+echo "Get Latest Version Number..."
+{
+wget --output-document=node-updater.html https://nodejs.org/dist/latest/
+
+ARCH=uname -m
+
+if [$ARCH = x86_64]
+then
+	grep -o '>node-v.*-linux-x64.tar.gz' node-updater.html > node-cache.txt 2>&1
+
+	VER=$(grep -o 'node-v.*-linux-x64.tar.gz' node-cache.txt)
+else
+	grep -o '>node-v.*-linux-x64.tar.gz' node-updater.html > node-cache.txt 2>&1
+	
+	VER=$(grep -o 'node-v.*-linux-x64.tar.gz' node-cache.txt)
+fi
+rm ./node-cache.txt
+rm ./node-updater.html
+} &> /dev/null
+
+echo "Done"
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo "Downloading latest stable Version $VER..."
+{
+wget https://nodejs.org/dist/latest/$VER
+} &> /dev/null
+
+echo "Done"
+
+echo "Installing..."
+cd /usr/local && sudo tar --strip-components 1 -xzf $DIR/$VER
+
+rm $DIR/$VER
+
+echo "Finished installing!"
